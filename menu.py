50b3cbfaa9f39f3431b7a22865a0ef14aa339d25
Kalyca Nazhifa Salwa <156705982+kalycans@users.noreply.github.com>
2025-05-19 14:06:16 +0700

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,9 @@
+# menu.py
+
+def tampilkan_menu():
+    print("\n===== Aplikasi Organisasi Mahasiswa =====")
+    print("1. Lihat daftar organisasi")
+    print("2. Lihat informasi organisasi")
+    print("3. Daftar ke organisasi")
+    print("4. Lihat data pendaftar")
+    print("5. Keluar")
